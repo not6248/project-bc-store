6c43201b3547f1e7393c38833f9326a702da1e48
AEKKAPOB PANGTAN <96365700+not6248@users.noreply.github.com>
2024-05-08 18:16:03 +0700

    add try catch

--- admin/page/product/delete.php
+++ admin/page/product/delete.php
@@ -1,21 +1,31 @@
 <?php
     if (isset($_GET['product_id']) && !empty($_GET['product_id'])) {
         $sql = "DELETE FROM product_tb WHERE product_id=" . $_GET['product_id'];
-        $result = mysqli_query($conn, $sql);
-        $img = $_GET['img'];
-        if ($result) {
-            if(!empty($img)){
-                unlink('upload/product/' . $img);
+        try {
+            $result = mysqli_query($conn, $sql);
+            $img = $_GET['img'];
+            if ($result) {
+                if(!empty($img)){
+                    unlink('upload/product/' . $img);
+                }
+                $alert = '<script>';
+                $alert .= 'alert("ลบข้อมูลสำเร็จ");';
+                $alert .= 'window.location.href = "?page=product";';
+                $alert .= '</script>';
+                echo $alert;
+                exit();
+            } else {
+                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
             }
+        } catch (\Throwable $e) {
             $alert = '<script>';
-            $alert .= 'alert("ลบข้อมูลสำเร็จ");';
+            $alert .= 'alert("เกิดข้อผิดพลาด");';
             $alert .= 'window.location.href = "?page=product";';
             $alert .= '</script>';
             echo $alert;
             exit();
-        } else {
-            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
         }
+
     }
     mysqli_close($conn);
 ?>
